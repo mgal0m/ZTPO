e7b7f94996ce6eef3d9ca2b68e5c66855b7738f0
Marcin Gal <mgal0m@gmail.com>
2016-10-28 20:58:12 +0200

    all + libs

---
+++ Backend/GetUserPublications.py
@@ -0,0 +1,20 @@
+import httplib2
+from bs4 import BeautifulSoup
+import re
+
+class GetUserPublications():
+    basicUrl1 = "https://suw.biblos.pk.edu.pl/"
+    basicUrl2 = "ajax_displaySearchResults&sId=16453-"
+    basicUrl3 = "&rsAt="
+    source = ""
+    userId = ""
+
+    def addId(self, userId):
+        self.userId = userId
+
+    
+
+
+    
+        
+          
