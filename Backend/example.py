163919dd0affef4e609c1e8700e0ff06b0a927a5
Marcin Gal <mgal0m@gmail.com>
2016-10-28 18:08:28 +0200

    get id class

---
+++ Backend/example.py
@@ -0,0 +1,49 @@
+import httplib2
+from bs4 import BeautifulSoup
+import re
+
+class GetUserId():
+    basicUrl = "https://suw.biblos.pk.edu.pl/browseByAuthor&letter="
+    name = ""
+    surname = ""
+    firstLetter = ""
+    source = ""
+
+    def addSurname(self, surname, name):
+        self.surname = surname
+        self.name = name
+        self.firstLetter = surname[0]
+
+    def getSource(self):
+        http = httplib2.Http()
+        status, response = http.request(self.basicUrl+self.firstLetter)
+        soup = BeautifulSoup(response, 'html.parser')
+        self.source = soup
+
+    def findIdBySurname(self):
+        href = re.compile('^userHomepage&uId')
+        idUrl = ""
+        for a in self.source.find_all('a', href=href):
+            text = str(a.contents)
+            text = text[0:30]
+            if self.surname in text and self.name in text:
+                print("jest")
+                idUrl = a['href']
+                x = re.compile('\d+')
+                x = re.findall(x, idUrl)
+                return x[0]
+                break
+
+    def getId(self, surname, name):
+        self.addSurname(surname, name)
+        self.getSource()
+        id = self.findIdBySurname()
+        return id
+        
+        
+            
+
+if __name__ == "__main__":
+    x = GetUserId()
+    id = x.getId("Góra", "Marta")
+    print(id)
