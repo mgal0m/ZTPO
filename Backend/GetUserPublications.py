6b1654101fd4a1af728874841e0b8f343b5e7fb6
Marcin Gal <mgal0m@gmail.com>
2016-10-28 22:29:01 +0200

    working version

--- Backend/GetUserPublications.py
+++ Backend/GetUserPublications.py
@@ -14,15 +14,17 @@ class GetUserPublications():
 
     def getSource(self):
         wd = webdriver.Chrome('libs/chromedriver.exe') #later switch to phantomjs
-        wd.get(BASE_URL+PUB_NUM_URL+str(self.userId))
-        #wd.get(BASE_URL+PUBLICATIONS_URL+str(self.userId)+"&rsAt="+"0")
-        #this.source = wd.page_source()
+        wd.get(BASE_URL+PUB_NUM_URL+str(self.userId)+"&rel=BPP-author")
+        wd.get(BASE_URL+PUBLICATIONS_URL+str(self.userId)+"&rsAt="+"0")
+        self.source = wd.page_source
 
 if __name__ == "__main__":
     x = GetUserPublications()
     x.addId(3322)
     x.getSource()
-    #print(x.source)
+    print("click any key than enter")
+    d = input()
+    print(x.source)
 
     
 
