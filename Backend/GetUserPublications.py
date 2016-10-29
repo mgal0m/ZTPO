1d14930b250ae22a4d5affc609456e60e81b7cf7
Marcin Gal <mgal0m@gmail.com>
2016-10-29 15:41:52 +0200

    little shitty update

--- Backend/GetUserPublications.py
+++ Backend/GetUserPublications.py
@@ -1,4 +1,3 @@
-import httplib2
 from bs4 import BeautifulSoup
 from selenium import webdriver
 import re
@@ -16,7 +15,22 @@ class GetUserPublications():
         wd = webdriver.Chrome('libs/chromedriver.exe') #later switch to phantomjs
         wd.get(BASE_URL+PUB_NUM_URL+str(self.userId)+"&rel=BPP-author")
         wd.get(BASE_URL+PUBLICATIONS_URL+str(self.userId)+"&rsAt="+"0")
-        self.source = wd.page_source
+        self.source = BeautifulSoup(wd.page_source, 'html.parser')
+
+
+    def getPublications(self):
+        text = self.source.find_all(text=True)
+        typeIndex = []
+        title = []
+        typeName = []
+        for i, j in enumerate(text):
+            if j==' typ: ':
+                typeIndex.append(i)
+                title.append(text[i-1])
+                typeName.append(text[i+1])
+        return title, typeName
+       
+
 
 if __name__ == "__main__":
     x = GetUserPublications()
@@ -24,9 +38,18 @@ if __name__ == "__main__":
     x.getSource()
     print("click any key than enter")
     d = input()
-    print(x.source)
+    a, b = x.getPublications()
+    print(a)
+    print(b)
 
-    
+
+"""
+x = soup.find_all(text=True)
+for i, j in enumerate(x):
+	if j==' Punktacja czasopisma na Liście MNiSW: ':
+		print(i)
+
+   """ 
 
 
     
